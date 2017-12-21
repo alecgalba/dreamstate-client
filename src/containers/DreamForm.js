458f11ca0cebb6a8821f7afb5607a8f2d9714fc5
alecgalba <alecgalba@gmail.com>
2017-12-21 16:34:09 -0500

    added actions, comp, cont, reduc folders and started dream container

---
+++ src/containers/DreamForm.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { updateDreamFormData } from '../actions/dreamForm';
+import { createDream } from '../actions/dreamActions';
+import FormError from '../components/FormError';
+
+class DreamForm extends Component {
+  
+}
