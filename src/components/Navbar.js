a491a700a170caa317ad59e44253a4c631639b5c
alecgalba <alecgalba@gmail.com>
2017-12-22 10:03:09 -0500

    built navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Menu, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div>
+      <Menu>
+        <Menu.Item header>Welcome To DreamState</Menu.Item>
+        <Menu.Item as={Link} to='/'>
+          <Icon name="home" />
+        </Menu.Item>
+        <Menu.Item as={Link} to='/dreams' name='Your Dreams'></Menu.Item>
+        <Menu.Item as={Link} to='/dreams/new' name='Log New Dream'></Menu.Item>
+        <Menu.Item as={Link} to='/about' name='About Page'></Menu.Item>
+        <Menu.Item as={Link} to='/contact' name='Contact Page'>
+          <Icon name='mail' />
+        </Menu.Item>
+      </Menu>
+    </div>
+  )
+}
+
+export default Navbar;
