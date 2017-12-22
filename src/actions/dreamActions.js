719c3d482677a219f732a1d070fd3df8d58a3ca7
alecgalba <alecgalba@gmail.com>
2017-12-22 09:04:04 -0500

    built dream actions

---
+++ src/actions/dreamActions.js
@@ -0,0 +1,117 @@
+import { resetDreamForm } from './dreamForm';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+//Action Creators
+export const setDreams = dreams => {
+  return {
+    type: 'GET_DREAMS',
+    dreams
+  }
+}
+
+export const addDream = dream => {
+  return {
+    type: 'CREAT_DREAM',
+    dream
+  }
+}
+
+export const removeDream = dream => {
+  return {
+    type: 'REMOVE_DREAM',
+    dream
+  }
+}
+
+export const addLikes = dream => {
+  return {
+    type: 'LIKE_DREAM',
+    dream
+  }
+}
+
+//Async actions
+export const getDreams = () => {
+  return dispatch => {
+    return fetch(`${API_URL}/recipes`)
+      .then(res => res.json())
+    .then(dreams => {
+      dispatch(setDreams(dreams))
+    })
+    .catch(error => console.log(error));
+  }
+}
+
+export const fetchDream = (dreamId) => {
+  return dispatch => {
+    return fetch(`${API_URL}/dreams/${dreamId}`)
+      .then(response => response.json())
+      .then(dream => {
+        dispatch(setDreams([dream]));
+      })
+      .catch(error => console.log(error));
+  }
+}
+
+export const createDream = (dream, routerHistory) => {
+  return dispatch => {
+    return fetch(`${API_URL}/dreams`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({dream: dream})
+    })
+    .then(handleErrors)
+    .then(response => response.json())
+    .then(dream => {
+      dispatch(addDream(dream))
+      dispatch(resetDreamForm())
+      routerHistory.replace(`/dreams/${dream.id}`)
+    })
+    .catch(error => {
+      dispatch({type: 'error'})
+      routerHistory.replace(`/recipes/new`)
+    })
+  }
+}
+
+export const deleteDream = (dreamId, routerHistory) => {
+  return dispatch => {
+    return fetch(`${API_URL}/dreams/${dreamId}`, {
+      methos: 'DELETE'
+    })
+    .then(response => {
+      dispatch(removeDream(dreamId));
+      routerHistory.replace('/dreams')
+    })
+    .catch(error => console.log(error))
+  }
+}
+
+export const likeDream = (dream, dreams) => {
+  const updateDream = Object.assign(...dream, { likes: dream.likes + 1})
+  return dispatch => {
+    return fetch(`${API_URL}/dreams/${dream.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({dream: updateDream})
+    })
+    .then(response => response.json())
+    .then(dream => {
+      dispatch(addLikes(recipe))
+      dispatch(addLikes(recipes))
+    })
+    .catch(error => console.log(error))
+  }
+}
+
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statustText);
+  }
+  return response;
+}
