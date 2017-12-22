2dd26b1aebe748de1cd6613fcfa5a81c23551381
alecgalba <alecgalba@gmail.com>
2017-12-22 09:45:06 -0500

    built about page

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='about'>
+      <h1>About DreamState</h1>
+      <h3>I built DreamState as a way to log my dreams during a transitional period of my life. I noticed that my dreams represented a wide range of emotions and involved many different characters from my past and sometimes characters who I had never met before. I began to write them into my notebook but then decided that creating an application for me and others to log their dreams could be beneficial in deciphering this phenomenom.</h3>
+      <h3>Dreaming is such a wonderful and mysterious part of life so I hope everyone can find comfort in reading their dreams from the past and finding commonalities.</h3>
+      <h3>Eventually I will incorporate a social feed in DreamState so you can see what your friends are dreaming about too!</h3>
+    </div>
+  )
+}
+
+export default About;
