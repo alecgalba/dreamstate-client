116f898dd40bc3985e509f2f2fdb0a00d36b0165
alecgalba <alecgalba@gmail.com>
2017-12-22 10:22:36 -0500

    modified app.js to include routes and structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import Navbar from './components/Navbar';
+import Home from './component/Home';
+import Dreams from './container/Dreams';
+import DreamForm from './containers/DreamForm';
+import DreamShow from './containers/DreamShow';
+import About from './components/About';
+import Contact from './components/Contact';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <div>
+            <Navbar />
+            <Container>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/dreams' component={Dreams} />
+                <Route exact path='/dreams/new' component={DreamForm} />
+                <Route exact path='/dreams/:dreamId' component={DreamShow} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/contact' component={Contact} />
+              </Switch>
+            </Container>
+          </div>
+        </Router>
       </div>
     );
   }
