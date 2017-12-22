b92f9f8b43d4da78cc81bdf0c421853a1ede1bf1
alecgalba <alecgalba@gmail.com>
2017-12-22 08:44:25 -0500

    built dreams container

---
+++ src/containers/Dreams.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connnect } from 'react-redux';
+import '../App.css';
+import DreamCard from '../components/DreamCard';
+import DreamForm from './DreamForm';
+import DreamShow from './DreamShow';
+import { getDreams } from '../actions/dreamActions';
+import { Switch, Route } from 'react-router-dom';
+import { likeDream } from '../actions/dreamActions';
+
+class Dream extends Component {
+
+  componentDidMount() {
+    this.props.getDreams()
+  }
+
+  render() {
+    const { dreams, match } = this.props;
+    const sortedDreams = dreams.sort(function(a,b) {
+      return b.likes - a.like;
+    })
+
+    return (
+      <div>
+        <Switch>
+        <Route exact path={match.url}
+          render={() =>
+        <div className='DreamsContainer'>
+        <h1 className='dreamName'>Dreams</h1>
+          {sortedDreams.map(dream => <DreamCard key={dream.id} dream={dream} />)}
+        </div>
+      }
+      />
+      <Route exact path='/dreams/new' component={DreamForm}/>
+          <Route
+            path='/dreams/:dreamId'
+            component={DreamShow}
+          />
+        </Switch>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return ({
+    dreams: state.dreams
+  })
+}
+
+export default connect(mapStateToProps, {getDreams, likeDream})(Dreams);
