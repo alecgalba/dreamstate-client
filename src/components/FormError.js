e37ace11fe2a8575759ae232aa677b4ba99daa34
alecgalba <alecgalba@gmail.com>
2017-12-22 09:55:17 -0500

    built form error component

---
+++ src/components/FormError.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FormError = () => {
+  const style = { color: 'red' }
+
+  return (
+    <p style={style}>Error: Please complete the form to log your DreamState</p>
+  )
+}
+
+export default FormError;
