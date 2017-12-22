0219ac01a3b81b6ff2656fd36738ac2d3299a4f0
alecgalba <alecgalba@gmail.com>
2017-12-22 11:44:59 -0500

    capitalized your

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,14 +5,13 @@ const Home = () => {
   return (
     <div className='home'>
     <h1> Welcome To DreamState! </h1>
-    <img src=/* find an image */ alt='home image' />
-    <h3>We welcome you to log your dreams every night including the number of hours slept and a brief description of the events.</h3>
+    <h3>We welcome you to log your dreams from the previous nights including the number of hours slept and a brief description of the events.</h3>
     <br />
     <h3>Hopefully, you are able to find patterns in your dreams that could lead to revelations about your current life status, your goals for your future, or even your circumstances of your past.</h3>
     <br />
-    <h3>You can also "like" your dreams as you re-read them and they will be ordered by the number of likes on your Dreams page.</h3>
+    <h3>You can also "like" your dreams as you re-read them and they will be ordered by the number of likes on Your Dreams page.</h3>
 
-    </h1>Happy Dreaming!</h1>
+    <h1>Happy Dreaming!</h1>
     </div>
   )
 }
