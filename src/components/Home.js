7c532a1ac7b53f00aa10f0ee6d91793b48499e05
alecgalba <alecgalba@gmail.com>
2017-12-22 09:38:34 -0500

    built home page

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../App.css';
+
+const Home = () => {
+  return (
+    <div className='home'>
+    <h1> Welcome To DreamState! </h1>
+    <img src=/* find an image */ alt='home image' />
+    <h3>We welcome you to log your dreams every night including the number of hours slept and a brief description of the events.</h3>
+    <br />
+    <h3>Hopefully, you are able to find patterns in your dreams that could lead to revelations about your current life status, your goals for your future, or even your circumstances of your past.</h3>
+    <br />
+    <h3>You can also "like" your dreams as you re-read them and they will be ordered by the number of likes on your Dreams page.</h3>
+
+    </h1>Happy Dreaming!</h1>
+    </div>
+  )
+}
+
+export default Home;
