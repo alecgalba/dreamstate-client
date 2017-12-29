076a9a7ccd9071041be727b270e0e08399ce64fa
alecgalba <alecgalba@gmail.com>
2017-12-29 11:06:01 -0500

    added top dreams feature for most popular and least popular dreams

---
+++ src/components/TopDreams.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import '../App.css';
+import DreamCard from '../components/DreamCard';
+// import DreamForm from '../containers/DreamForm';
+// import DreamShow from '../containers/DreamShow';
+import { getDreams } from '../actions/dreamActions';
+import { Route } from 'react-router-dom';
+import { likeDream } from '../actions/dreamActions';
+
+class TopDreams extends Component {
+
+  componentDidMount() {
+    this.props.getDreams()
+  }
+
+  render() {
+    const { dreams, match } = this.props;
+    const sortedDreams = dreams.sort(function(a, b) {
+            return b.likes - a.likes;
+          })
+
+    return (
+      <div>
+      <Route exact path={match.url}
+       render={() =>
+      <div className="DreamsContainer">
+      <h1 className="dreamName">Most And Least Popular Dreams</h1>
+        {sortedDreams.map(dream => <DreamCard key={dream.id} dream={dream} /> ).slice(0, 1)}
+        {sortedDreams.map(dream => <DreamCard key={dream.id} dream={dream} /> ).slice(-1)}
+      </div>
+    }
+    />
+     </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return ({
+    dreams: state.dreams
+  })
+}
+
+export default connect(mapStateToProps, {getDreams, likeDream})(TopDreams);
