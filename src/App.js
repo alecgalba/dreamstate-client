99480aea9f273355f812bd4f3ac7db2d4a995b44
alecgalba <alecgalba@gmail.com>
2017-12-29 11:06:33 -0500

    modified routes and added top dreams route

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import DreamForm from './containers/DreamForm';
 import DreamShow from './containers/DreamShow';
 import About from './components/About';
 import Contact from './components/Contact';
+import TopDreams from './components/TopDreams';
 
 class App extends Component {
   render() {
@@ -20,6 +21,7 @@ class App extends Component {
             <Container>
               <Switch>
                 <Route exact path='/' component={Home} />
+                <Route exact path='/dreams/top' component={TopDreams} />
                 <Route exact path='/dreams' component={Dreams} />
                 <Route exact path='/dreams/new' component={DreamForm} />
                 <Route exact path='/dreams/:dreamId' component={DreamShow} />
